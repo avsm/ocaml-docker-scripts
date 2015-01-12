81cfc4ea9e139bafb9902489c790bf17634b7fde
Anil Madhavapeddy <anil@recoil.org>
2015-01-12 17:35:15 +0000

    fix cron generation of html

--- bulk-build/cron.sh
+++ bulk-build/cron.sh
@@ -4,4 +4,8 @@ JOBS=15
 make clean
 make -j ${JOBS} depend
 make -j ${JOBS} -f Makefile.bulk
-./generate.ml -g
+./generate_html.ml -g
+repoid=`cat opam-repo-rev`
+mkdir -p archive/$repoid
+mv logs archive/$repoid/
+cp index.html theme.css archive/$repoid/
